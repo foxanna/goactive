7207c2c50ff3076c55639b0d9a0f8d5c57d809ca
Anna Domashych <anna.domashych@gmail.com>
2020-04-28 11:31:51 +0300

    Change MaterialApp title

--- lib/application.dart
+++ lib/application.dart
@@ -7,7 +7,7 @@ class GoActiveApplication extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) => MaterialApp(
-        title: 'Flutter Demo',
+        title: 'Go Active',
         theme: createTheme(),
         home: const FeedPage(),
       );
