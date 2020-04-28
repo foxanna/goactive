71ce1f643d98143d248c1242c83dc60276eb4a7b
Anna Domashych <anna.domashych@gmail.com>
2020-04-28 20:41:48 +0300

    Setup Card theme

--- lib/styles/theme.dart
+++ lib/styles/theme.dart
@@ -5,6 +5,16 @@ ThemeData createTheme() {
     brightness: Brightness.light,
     primarySwatch: Colors.pink,
     visualDensity: VisualDensity.adaptivePlatformDensity,
+    cardTheme: CardTheme(
+      clipBehavior: Clip.hardEdge,
+      color: Colors.white,
+      shape: const RoundedRectangleBorder(
+        borderRadius: BorderRadius.all(
+          Radius.circular(16.0),
+        ),
+      ),
+      margin: const EdgeInsets.all(16.0),
+    ),
   );
 
   return base;
