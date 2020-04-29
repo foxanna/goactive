ebba19fc99b3fb48dc5c50678ba88282d1b2ad6a
Anna Domashych <anna.domashych@gmail.com>
2020-04-29 15:00:57 +0300

    Implement ImageBottomGradient widget

---
+++ lib/screens/widgets/image_bottom_gradient.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class ImageBottomGradient extends StatelessWidget {
+  final Widget child;
+
+  const ImageBottomGradient({Key key, this.child}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) => Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: const [Colors.transparent, Color(0xBD000000)],
+            stops: [0.7, 1.0],
+          ),
+        ),
+        child: child,
+      );
+}
