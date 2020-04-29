ce382f3e93074cafd68f7db274d0603b35cde823
Anna Domashych <anna.domashych@gmail.com>
2020-04-29 16:49:40 +0300

    Implement UnimplementedPage stub

---
+++ lib/application/unimplemented_page.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:goactive/widgets/error_message.dart';
+import 'package:goactive/widgets/page_background.dart';
+
+class UnimplementedPage extends StatelessWidget {
+  const UnimplementedPage({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) => Scaffold(
+        body: PageBackground(
+          child: Center(
+            child: Text(
+              'This page is under construction',
+              textAlign: TextAlign.center,
+              style: Theme.of(context).textTheme.subtitle1.copyWith(
+                    color: Colors.white,
+                  ),
+            ),
+          ),
+        ),
+      );
+}
