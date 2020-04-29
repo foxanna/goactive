91fa8039bcb05606ec6f59cc4eee5eba0acc4594
Anna Domashych <anna.domashych@gmail.com>
2020-04-29 16:45:09 +0300

    Implement SplashPage

---
+++ lib/application/splash_page.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:goactive/widgets/page_background.dart';
+
+class SplashPage extends StatelessWidget {
+  const SplashPage({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) => const Scaffold(
+        body: PageBackground(
+          child: Center(
+            child: Icon(
+              Icons.directions_run,
+              color: Colors.white,
+              size: 80.0,
+            ),
+          ),
+        ),
+      );
+}
