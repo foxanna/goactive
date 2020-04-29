44fb0f22f99375b56054cc4e0df8556d5a4cea61
Anna Domashych <anna.domashych@gmail.com>
2020-04-29 11:24:35 +0300

    Refactor tests

--- test/api/services/feed/feed_api_service_test.dart
+++ test/api/services/feed/feed_api_service_test.dart
@@ -1,6 +1,7 @@
 import 'dart:convert';
 
 import 'package:flutter_test/flutter_test.dart';
+import 'package:goactive/api/models/activity.dart';
 import 'package:goactive/api/services/feed/feed_api_service.dart';
 import 'package:mockito/mockito.dart';
 
@@ -8,22 +9,51 @@ import '../../../data_generator.dart';
 import '../../../mocks.dart';
 
 void main() {
-  test('Loads activity feed', () async {
-    // Arrange
-    final expectedFeed = Iterable<int>.generate(20)
-        .map((id) => generateTestActivity(id))
-        .toList();
+  group('FeedApiService.getFeed tests', () {
+    final execute = ({
+      String apiResponse,
+      String lastActivityId,
+      List<Activity> expectedFeed,
+    }) async {
+      // Arrange
+      final httpServiceMock = HttpServiceMock();
+      when(httpServiceMock
+              .get(Uri(path: '/feed', query: 'last=${lastActivityId ?? ''}')))
+          .thenAnswer((_) => Future.value(jsonEncode(expectedFeed)));
 
-    final httpServiceMock = HttpServiceMock();
-    when(httpServiceMock.get(Uri(path: '/feed', query: 'last=')))
-        .thenAnswer((_) => Future.value(jsonEncode(expectedFeed)));
+      final feedService = FeedApiService(httpService: httpServiceMock);
 
-    final feedService = FeedApiService(httpService: httpServiceMock);
+      // Act
+      final feed = await feedService.getFeed(lastActivityId: lastActivityId);
 
-    // Act
-    final feed = await feedService.getFeed();
+      // Assert
+      expect(feed, expectedFeed);
+    };
 
-    // Assert
-    expect(feed, expectedFeed);
+    test('Loads first portion of activity feed', () async {
+      // Arrange
+      final expectedFeed = generateTestActivities(20);
+
+      // Act
+      // Assert
+      await execute(
+        apiResponse: jsonEncode(expectedFeed),
+        lastActivityId: null,
+        expectedFeed: expectedFeed,
+      );
+    });
+
+    test('Loads Nth portion of activity feed', () async {
+      // Arrange
+      final expectedFeed = generateTestActivities(20);
+
+      // Act
+      // Assert
+      await execute(
+        apiResponse: jsonEncode(expectedFeed),
+        lastActivityId: 'test',
+        expectedFeed: expectedFeed,
+      );
+    });
   });
 }
--- test/services/feed/feed_repository_test.dart
+++ test/services/feed/feed_repository_test.dart
@@ -1,5 +1,4 @@
 import 'package:flutter_test/flutter_test.dart';
-import 'package:goactive/api/models/activity.dart';
 import 'package:goactive/services/feed/feed_repository.dart';
 import 'package:mockito/mockito.dart';
 
@@ -7,111 +6,101 @@ import '../../data_generator.dart';
 import '../../mocks.dart';
 
 void main() {
-  const feedPortionSize = 20;
+  group('FeedRepository.feed tests', () {
+    test('Streams activity feed', () async {
+      // Arrange
+      final expectedFeed = generateTestActivities(20);
 
-  test('Streams activity feed', () async {
-    // Arrange
-    final expectedFeed = Iterable<int>.generate(feedPortionSize)
-        .map((id) => generateTestActivity(id))
-        .toList();
+      final feedApiServiceMock = FeedApiServiceMock();
+      when(feedApiServiceMock.getFeed(lastActivityId: null))
+          .thenAnswer((_) => Future.value(expectedFeed));
 
-    final feedApiServiceMock = FeedApiServiceMock();
-    when(feedApiServiceMock.getFeed(lastActivityId: null))
-        .thenAnswer((_) => Future.value(expectedFeed));
+      final feedRepository = FeedRepository(apiService: feedApiServiceMock);
 
-    final feedRepository = FeedRepository(apiService: feedApiServiceMock);
+      // Act
+      await feedRepository.requestMoreData();
 
-    // Act
-    await feedRepository.requestMoreData();
+      // Assert
+      expect(feedRepository.feed, emits(expectedFeed));
+    });
 
-    // Assert
-    expect(feedRepository.feed, emits(expectedFeed));
-  });
+    test('Streams exception', () async {
+      // Arrange
+      final expectedException = Exception();
 
-  test('Streams error', () async {
-    // Arrange
-    final expectedException = Exception();
+      final feedApiServiceMock = FeedApiServiceMock();
+      when(feedApiServiceMock.getFeed(lastActivityId: null))
+          .thenAnswer((_) => Future.error(expectedException));
 
-    final feedApiServiceMock = FeedApiServiceMock();
-    when(feedApiServiceMock.getFeed(lastActivityId: null))
-        .thenAnswer((_) => Future.error(expectedException));
+      final feedRepository = FeedRepository(apiService: feedApiServiceMock);
 
-    final feedRepository = FeedRepository(apiService: feedApiServiceMock);
+      // Act
+      await feedRepository.requestMoreData();
 
-    // Act
-    await feedRepository.requestMoreData();
+      // Assert
+      expect(feedRepository.feed, emitsError(expectedException));
+    });
 
-    // Assert
-    expect(feedRepository.feed, emitsError(expectedException));
-  });
+    test('Streams accumulated activity feed', () async {
+      // Arrange
+      final expectedFeed1 = generateTestActivities(20);
+      final expectedFeed2 = generateTestActivities(expectedFeed1.length,
+          firstId: expectedFeed1.length);
 
-  test('Streams accumulated activity feed', () async {
-    // Arrange
-    final expectedFeed1 = Iterable<int>.generate(feedPortionSize)
-        .map((id) => generateTestActivity(id))
-        .toList();
-    final expectedFeed2 =
-        Iterable<int>.generate(feedPortionSize, (x) => x + feedPortionSize)
-            .map((id) => generateTestActivity(id))
-            .toList();
+      final feedApiServiceMock = FeedApiServiceMock();
+      when(feedApiServiceMock.getFeed(lastActivityId: null))
+          .thenAnswer((_) => Future.value(expectedFeed1));
+      when(feedApiServiceMock.getFeed(lastActivityId: expectedFeed1.last.id))
+          .thenAnswer((_) => Future.value(expectedFeed2));
 
-    final feedApiServiceMock = FeedApiServiceMock();
-    when(feedApiServiceMock.getFeed(lastActivityId: null))
-        .thenAnswer((_) => Future.value(expectedFeed1));
-    when(feedApiServiceMock.getFeed(lastActivityId: expectedFeed1.last.id))
-        .thenAnswer((_) => Future.value(expectedFeed2));
+      final feedRepository = FeedRepository(apiService: feedApiServiceMock);
 
-    final feedRepository = FeedRepository(apiService: feedApiServiceMock);
+      // Act
+      await feedRepository.requestMoreData();
+      await feedRepository.requestMoreData();
 
-    // Act
-    await feedRepository.requestMoreData();
-    await feedRepository.requestMoreData();
+      // Assert
+      expect(
+          feedRepository.feed,
+          emitsInOrder([
+            expectedFeed1,
+            expectedFeed1.followedBy(expectedFeed2),
+          ]));
+    });
 
-    // Assert
-    expect(
-        feedRepository.feed,
-        emitsInOrder([
-          expectedFeed1,
-          expectedFeed1.followedBy(expectedFeed2),
-        ]));
-  });
+    test('Streams accumulated activity feed even after exception', () async {
+      // Arrange
+      final expectedFeed1 = generateTestActivities(20);
+      final expectedException = Exception();
+      final expectedFeed2 = generateTestActivities(expectedFeed1.length,
+          firstId: expectedFeed1.length);
 
-  test('Streams accumulated activity feed even after exception', () async {
-    // Arrange
-    final expectedFeed1 = Iterable<int>.generate(feedPortionSize)
-        .map((id) => generateTestActivity(id))
-        .toList();
-    final expectedException = Exception();
-    final expectedFeed2 =
-        Iterable<int>.generate(feedPortionSize, (x) => x + feedPortionSize)
-            .map((id) => generateTestActivity(id))
-            .toList();
+      final secondFeedCallAnswers = [
+        () async => throw expectedException,
+        () async => expectedFeed2,
+      ];
 
-    final secondFeedCallAnswers = [
-      () async => throw expectedException,
-      () async => expectedFeed2,
-    ];
+      final feedApiServiceMock = FeedApiServiceMock();
+      when(feedApiServiceMock.getFeed(lastActivityId: null))
+          .thenAnswer((_) => Future.value(expectedFeed1));
+      when(feedApiServiceMock.getFeed(lastActivityId: expectedFeed1.last.id))
+          .thenAnswer((_) => secondFeedCallAnswers.removeAt(0)());
 
-    final feedApiServiceMock = FeedApiServiceMock();
-    when(feedApiServiceMock.getFeed(lastActivityId: null))
-        .thenAnswer((_) => Future.value(expectedFeed1));
-    when(feedApiServiceMock.getFeed(lastActivityId: expectedFeed1.last.id))
-        .thenAnswer((_) => secondFeedCallAnswers.removeAt(0)());
+      final feedRepository = FeedRepository(apiService: feedApiServiceMock);
 
-    final feedRepository = FeedRepository(apiService: feedApiServiceMock);
+      // Act
+      await feedRepository.requestMoreData();
+      await feedRepository.requestMoreData();
+      await feedRepository.requestMoreData();
 
-    // Act
-    await feedRepository.requestMoreData();
-    await feedRepository.requestMoreData();
-    await feedRepository.requestMoreData();
-
-    // Assert
-    expect(
-        feedRepository.feed,
-        emitsInOrder([
-          expectedFeed1,
-          emitsError(expectedException),
-          expectedFeed1.followedBy(expectedFeed2),
-        ]));
+      // Assert
+      expect(
+          feedRepository.feed,
+          emitsInOrder([
+            expectedFeed1,
+            emitsError(expectedException),
+            expectedFeed1.followedBy(expectedFeed2),
+          ]));
+    });
   });
 }
