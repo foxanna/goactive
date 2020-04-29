8d26dea27fde4c1c4827e849d3d4b57ccb661fe6
Anna Domashych <anna.domashych@gmail.com>
2020-04-29 09:43:59 +0300

    Implement PageBackground widget

---
+++ lib/screens/widgets/page_background.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class PageBackground extends StatelessWidget {
+  final Widget child;
+
+  const PageBackground({Key key, this.child}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) => Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomRight,
+            colors: [
+              Colors.pink,
+              Colors.deepOrangeAccent,
+              Colors.pinkAccent,
+              Colors.deepOrange,
+            ],
+            stops: [
+              0.2,
+              0.5,
+              0.8,
+              1.0,
+            ],
+          ),
+        ),
+        child: child,
+      );
+}
