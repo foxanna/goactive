ca155e27aaa1490d7bf16ca2a0c732c6934a09ce
Anna Domashych <anna.domashych@gmail.com>
2020-04-29 16:52:15 +0300

    Display user info in app bar

--- lib/screens/feed/views/feed_page_app_bar.dart
+++ lib/screens/feed/views/feed_page_app_bar.dart
@@ -1,10 +1,33 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:goactive/authentication/bloc/authentication_bloc.dart';
+import 'package:goactive/screens/feed/styles/dimensions.dart';
+import 'package:goactive/styles/dimensions.dart';
 
 class FeedPageAppBar extends StatelessWidget {
   const FeedPageAppBar({Key key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) => SliverAppBar(
-        backgroundColor: Colors.transparent,
+  Widget build(BuildContext context) =>
+      BlocBuilder<AuthenticationBloc, AuthenticationState>(
+        builder: (context, state) => SliverAppBar(
+          backgroundColor: Colors.transparent,
+          title: state.maybeMap(
+            authenticated: (value) => Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Text(value.user.name),
+                SizedBox(width: defaultSpacing),
+                CircleAvatar(
+                  backgroundImage:
+                      CachedNetworkImageProvider(value.user.avatar),
+                  radius: userAvatarRadius * 1.5,
+                ),
+              ],
+            ),
+            orElse: () => Container(),
+          ),
+        ),
       );
 }
